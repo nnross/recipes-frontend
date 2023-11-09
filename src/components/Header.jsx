73549582b0cb18d034df1fb7d4f91ed43df06c34
Nelli Rossi <nnross@utu.fi>
2023-11-09 22:06:35 +0200

    header without mobile side bar

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import $ from 'jquery';
+
+/**
+ * The Header component.
+ * @property {String} className - Custom className if wanted. Default is header.
+ * @property {String} id - Custom id if wanted. Default is header.
+ * @returns Header.
+ */
+const Header = ({ className, id, openLogin }) => {
+  const openNav = () => {
+    $(`#${id}__nav__sidebar`).css('display', 'flex');
+  };
+  const closeNav = () => {
+    $(`#${id}__nav__sidebar`).css('display', 'none');
+  };
+  return (
+    <header className={className} id={id}>
+      <div className={`${className}__nav`} id={`${id}__nav`}>
+        <button className={`${className}__nav__button`} id={`${id}__nav__button`} onClick={openNav} type="button" aria-label="hamburger" />
+        <div className={`${className}__nav__icon`} />
+        <nav className={`${className}__nav__sidebar`} id={`${id}__nav__sidebar`}>
+          <button className={`${className}__nav__sidebar__close`} id={`${id}__nav__sidebar__close`} onClick={closeNav} type="button" aria-label="close" />
+          <a className={`${className}__nav__sidebar__home`} href="/home"> home </a>
+          <a className={`${className}__nav__sidebar__search`} href="/all"> search </a>
+          <a className={`${className}__nav__sidebar__settings`} href="/settings"> personal </a>
+          <button className={`${className}__nav__sidebar__logout`} type="button"> log out </button>
+        </nav>
+      </div>
+      <div className={`${className}__title`} id={`${id}__title`}>
+        <h4 className={`${className}__title__text`} id={`${id}__title__text`}>
+          RecipeBuddy
+        </h4>
+      </div>
+      <div className={`${className}__logIn`} id={`${id}__logIn`}>
+        <button className={`${className}__logIn__button`} id={`${id}__logIn__button`} onClick={openLogin} type="button">Log in</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
+Header.propTypes = {
+  className: propTypes.string,
+  id: propTypes.string,
+  openLogin: propTypes.func,
+};
+
+Header.defaultProps = {
+  className: 'header',
+  id: 'header',
+  openLogin: null,
+};
