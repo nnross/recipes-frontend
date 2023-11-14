437f7ab86c3a03e08efae126352f9aef6363e1cf
Nelli Rossi <nnross@utu.fi>
2023-11-14 10:58:25 +0200

    filter tests

--- src/tests/components/Filter.test.jsx
+++ src/tests/components/Filter.test.jsx
@@ -7,7 +7,7 @@ import Filter from '../../components/Filter';
 describe('Filter tests', () => {
   describe('Filter renders', () => {
     test('Filter render correctly', () => {
-      const component = render(<Filter id="test" name="testName" filters={['filter1', 'filter2']} />);
+      const component = render(<Filter id="test" title="testName" filters={['filter1', 'filter2']} />);
 
       const container = component.container.querySelector('#test');
       expect(container).not.toBeNull();
@@ -16,17 +16,17 @@ describe('Filter tests', () => {
 
       expect(component.getByRole('button', { name: 'testName' })).toBeVisible();
 
-      expect(component.getByRole('button', { name: 'filter1' })).not.toBeVisible();
-      expect(component.getByRole('button', { name: 'filter2' })).not.toBeVisible();
+      expect(component.queryByRole('button', { name: 'filter1' })).not.toBeInTheDocument();
+      expect(component.queryByRole('button', { name: 'filter2' })).not.toBeInTheDocument();
     });
   });
   describe('Filter functions work', () => {
     test('open filter and select sort works', async () => {
       const mockOnFilter = jest.fn();
-      const component = render(<Filter id="test" onFilter={mockOnFilter} name="testName" filters={['filter1', 'filter2']} />);
+      const component = render(<Filter id="test" selectFilter={mockOnFilter} title="testName" filters={['filter1', 'filter2']} />);
       const button = component.getByRole('button', { name: 'testName' });
-      expect(component.getByRole('button', { name: 'filter1' })).not.toBeVisible();
-      expect(component.getByRole('button', { name: 'filter2' })).not.toBeVisible();
+      expect(component.queryByRole('button', { name: 'filter1' })).not.toBeInTheDocument();
+      expect(component.queryByRole('button', { name: 'filter2' })).not.toBeInTheDocument();
 
       await userEvent.click(button);
 
@@ -36,12 +36,12 @@ describe('Filter tests', () => {
       await userEvent.click(component.getByRole('button', { name: 'filter1' }));
 
       expect(mockOnFilter.mock.calls).toHaveLength(1);
-      expect(mockOnFilter.mock.calls[0][0]).toBe('filter1');
+      expect(mockOnFilter.mock.calls[0][0]).toBe('testName-filter1');
 
       await userEvent.click(button);
 
-      expect(component.getByRole('button', { name: 'filter1' })).not.toBeVisible();
-      expect(component.getByRole('button', { name: 'filter2' })).not.toBeVisible();
+      expect(component.queryByRole('button', { name: 'filter1' })).not.toBeInTheDocument();
+      expect(component.queryByRole('button', { name: 'filter2' })).not.toBeInTheDocument();
     });
   });
 });
