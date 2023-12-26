ac5596ce3f21e94fcc4bd4ff3fc10a15960aed40
Iiro Partanen <iispar@utu.fi>
2023-12-26 16:42:00 +0200

    final tests for sprint7

--- src/tests/services/personService.test.jsx
+++ src/tests/services/personService.test.jsx
@@ -1,7 +1,7 @@
 import * as axios from 'axios';
 import '@testing-library/jest-dom/extend-expect';
 import { waitFor } from '@testing-library/react';
-import { getAccountData } from '../../services/personService';
+import { getAccountData, postAccountData, deleteAccountData } from '../../services/personService';
 import { user } from '../testData/account.json';
 
 const mockPost = jest.fn();
@@ -13,11 +13,51 @@ beforeEach(() => {
     mockPost(url, config);
     Promise.resolve(user);
   });
+
+  axios.put.mockImplementation((url, config) => {
+    mockPost(url, config);
+    Promise.resolve(user);
+  });
+
+  axios.delete.mockImplementation((url, config) => {
+    mockPost(url, config);
+    Promise.resolve(user);
+  });
 });
 
 describe('person tests', () => {
   test('getAccountData calls correctly', async () => {
     const res = getAccountData(0, 'token');
+    const config = {
+      headers: { Authorization: 'Bearer token' },
+    };
+
+    await waitFor(() => {
+      expect(res).toBe(user);
+
+      expect(mockPost.mock.calls).toHaveLength(1);
+      expect(mockPost.mock.calls[0][0]).toBe('URL');
+      expect(mockPost.mock.calls[0][1]).toBe(config);
+    });
+  });
+
+  test('postAccountData calls correctly', async () => {
+    const res = postAccountData(0, 'token');
+    const config = {
+      headers: { Authorization: 'Bearer token' },
+    };
+
+    await waitFor(() => {
+      expect(res).toBe(user);
+
+      expect(mockPost.mock.calls).toHaveLength(1);
+      expect(mockPost.mock.calls[0][0]).toBe('URL');
+      expect(mockPost.mock.calls[0][1]).toBe(config);
+    });
+  });
+
+  test('deleteAccountData calls correctly', async () => {
+    const res = deleteAccountData(0, 'token');
     const config = {
       headers: { Authorization: 'Bearer token' },
     };
